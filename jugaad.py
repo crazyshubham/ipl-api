1ef365282af46e44a03912ff2ddc02a7e006c3ca
crazyshubham <shubham2005kumarupadhyay@gmail.com>
2026-05-13 00:55:18 +0530

    Update jugaad.py

--- jugaad.py
+++ jugaad.py
@@ -21,8 +21,8 @@ balls = pd.read_csv(ipl_ball)
 ball_withmatch = balls.merge(matches, on='ID', how='inner').copy()
 ball_withmatch['BowlingTeam'] = ball_withmatch.Team1 + ball_withmatch.Team2
 ball_withmatch['BowlingTeam'] = ball_withmatch[['BowlingTeam', 'BattingTeam']].apply(lambda x: x.values[0].replace(x.values[1], ''), axis=1)
+ball_withmatch = ball_withmatch.rename(columns={'player_of_match': 'Player_of_Match'})
 batter_data = ball_withmatch[np.append(balls.columns.values, ['BowlingTeam', 'Player_of_Match'])]
-
 
 def team1vsteam2(team, team2):
     df = matches[((matches['Team1'] == team) & (matches['Team2'] == team2)) | (
@@ -119,7 +119,7 @@ def batsmanVsTeam(batsman, team, df):
 
 
 def batsmanAPI(batsman, balls=batter_data):
-    df = balls[balls.innings.isin([1, 2])]  # Excluding Super overs
+    df = balls[balls.innings.isin([1, 2])]  
     self_record = batsmanRecord(batsman, df=df)
     TEAMS = matches.Team1.unique()
     against = {team: batsmanVsTeam(batsman, team, df) for team in TEAMS}
